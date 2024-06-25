4c975e978ab8289645981c18292c60c4ffd9a030
pommejedusor <118850425+pommeJedusor@users.noreply.github.com>
2024-06-25 08:53:27 +0200

    getitem and fix getAvailableHorizontalDigits

--- solver.py
+++ solver.py
@@ -2,22 +2,25 @@ class SudokuGrid:
     def __init__(self, grid:str):
         self.grid = grid
 
+    def __getitem__(self, item):
+        return self.grid[item]
+
     def getFirstEmptySquareIndex(self)->int:
         return self.grid.find("0")
 
     def getAvailableHorizontalDigits(self, index:int)->list[str]:
         horizontal_start = index - index % 9
-        return self.grid[horizontal_start:horizontal_start+9]
+        return [digit for digit in self[horizontal_start:horizontal_start+9]]
 
     def getAvailableVerticalDigits(self, index:int)->list[str]:
         vertital_start = index % 9
-        return [self.grid[vertital_start + i*9] for i in range(9)]
+        return [self[vertital_start + i*9] for i in range(9)]
 
     def getAvailableSquareDigits(self, index:int)->list[str]:
         square_x_start = index - index % 3
         square_y_start = index - index % 27
         square_start = square_x_start + square_y_start
-        return [self.grid[square_start + i%3 + i//3*9] for i in range(9)]
+        return [self[square_start + i%3 + i//3*9] for i in range(9)]
 
     def getAvailableDigits(self, index:int)->list[str]:
         # "1" -> "9"
